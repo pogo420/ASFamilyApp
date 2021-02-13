a8ee8421a7bb40663a4beef28abf369107f4f98a
pogo420 <am0072008@gmail.com>
2021-02-13 11:56:42 +0530

    Family App setup

--- app/src/main/java/com/example/asfamilyapp/MasterActivity.java
+++ app/src/main/java/com/example/asfamilyapp/MasterActivity.java
@@ -2,7 +2,10 @@ package com.example.asfamilyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MasterActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MasterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_master);
+        findViewById(R.id.calc).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(MasterActivity.this, "Ola", Toast.LENGTH_SHORT).show(); // Classname.this --> for outer class
+                startActivity(new Intent(MasterActivity.this, CalculatorActivity.class));
+            }
+        });
     }
 }

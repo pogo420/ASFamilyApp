d10ac1dc81b8b7329ae39b32236608c39ec16be7
pogo420 <am0072008@gmail.com>
2021-02-13 22:00:45 +0530

    emi base

--- app/src/main/java/com/example/asfamilyapp/CalculatorActivity.java
+++ app/src/main/java/com/example/asfamilyapp/CalculatorActivity.java
@@ -2,7 +2,9 @@ package com.example.asfamilyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CalculatorActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        findViewById(R.id.emi).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(MasterActivity.this, "Ola", Toast.LENGTH_SHORT).show(); // Classname.this --> for outer class
+                startActivity(new Intent(CalculatorActivity.this, EmiCalcActivity.class));
+            }
+        });
     }
 }
